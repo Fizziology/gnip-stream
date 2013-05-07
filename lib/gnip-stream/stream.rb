75565a23d37fd765ae8121e7f9122befe1cd4564
Iouri Kostine <iouri@theablefew.com>
2013-05-07 18:17:12 -0500

    Handle replay

--- lib/gnip-stream/stream.rb
+++ lib/gnip-stream/stream.rb
@@ -28,6 +28,10 @@ module GnipStream
       @on_error = block
     end
 
+    def is_replay?
+      url.path.include?('replay')
+    end
+
     def connect
       EM.run do
         Net::HTTP.start( url.host, url.port, :use_ssl => url.scheme == 'https') do |http|
@@ -35,21 +39,26 @@ module GnipStream
             request = Net::HTTP::Get.new url.request_uri
             request.initialize_http_header(headers)
             request.basic_auth @username, @password
-            
+
             http.request(request) do |response|
               StringIO.open do |io|
                 response.read_body{ |chunk| process_chunk( chunk ) }
               end
             end
             
-            http.callback { 
-              handle_connection_close(http) 
-              EM.stop
-            }
-            http.errback { 
-              handle_error(http)
-              EM.stop
-            }
+            if is_replay?
+              puts "Replay Ended"
+              EM.stop_event_loop
+            else
+              http.callback { 
+                handle_connection_close(http) 
+                EM.stop
+              }
+              http.errback { 
+                handle_error(http)
+                EM.stop
+              }
+            end
           rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
             handle_error( http ) 
           end
@@ -60,7 +69,11 @@ module GnipStream
     def process_chunk(chunk)
       @processor.process(chunk)
       @processor.complete_entries.each do |entry|
-        EM.defer { @on_message.call(entry) }
+        if entry.starts_with?("{\"info")
+          puts entry
+        else
+          EM.defer { @on_message.call(entry) }
+        end
       end
     end
 
