81baee737dee77726ab04b7782e2a0ab434c419d
Spencer Markowski <spencer@theablefew.com>
2017-01-17 08:31:05 -0500

    Add customizable split pattern for JsonDataBuffer

--- lib/gnip-stream/json_stream.rb
+++ lib/gnip-stream/json_stream.rb
@@ -3,8 +3,8 @@ require 'gnip-stream/stream_delegate'
 module GnipStream
   class JsonStream
     include StreamDelegate
-    def initialize(url, username, password, headers={})
-      json_processor = JsonDataBuffer.new("\r\n", Regexp.new(/^\{.*\}\r\n/))
+    def initialize(url, username, password, headers={}, split_pattern = "\r\n")
+      json_processor = JsonDataBuffer.new(split_pattern, Regexp.new(/^\{.*\}#{split_pattern}/))
       @stream = Stream.new(url, username, password, json_processor, headers)
     end
   end
